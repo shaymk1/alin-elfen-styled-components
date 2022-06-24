24a90c4149791d9b7ad3227f98ac76109738dfe9
Mamosebetsi Kekae <mkekae@gmail.com>
2022-06-24 16:29:46 -0700

    installed cra

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import './App.css';
+import NavBar from 'NavBar';
+
+
+function App() {
+  return (
+		<div className="App">
+			<NavBar />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/NavBar.jsx
@@ -0,0 +1,15 @@
+
+
+import Img from './assets/logo/alin elefen logo-1.png'
+const NavBar = () => {
+	return (
+		<div>
+		<img src= {Img} alt="" />
+			NavBar
+			
+			
+		</div>
+	);
+};
+
+export default NavBar;
